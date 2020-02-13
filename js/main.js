fbf873089309a21dbd5816c8759dfb8d43f3b4af
Roman Torres <rtorres@envivent.com>
2020-02-13 13:19:01 -0800

    comment out jquery to change header w click or kd

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
-(function() {
-    $('#redbutton').on('click', function() {
+// (function() {
+//     $('#redbutton').on('click', function() {
+//         $('header').text('Red')
+//     });
+//     $('#greenbutton').on('click', function() {
+//         $('header').text('Green')
+//     });
+//     $('#bluebutton').on('click', function() {
+//         $('header').text('Blue')
+//     });
+// })();
+
+//JavaScript to change color on keydown
+/*function checkKeyPress(key) {
+    if (key.keyCode =="65") {
         $('header').text('Red')
-    });
-    $('#greenbutton').on('click', function() {
+    }
+    if (key.keyCode =="83") {
         $('header').text('Green')
-    });
-    $('#bluebutton').on('click', function() {
+    }
+    if (key.keyCode =="68") {
         $('header').text('Blue')
-    });
-})();
+    }
+}*/
+
+//JQuery to change color on keydown with too many ifs
+// $(document).on('keydown', function (e) {  
+//     if (e.keyCode == 65 ) {
+//         $('header').text('Red')
+//     }
+//     if (e.keyCode == 83 ) {
+//         $('header').text('Green')
+//     }
+//     if (e.keyCode == 68 ) {
+//         $('header').text('Blue')
+//     }
+// });
